2930d637ef7af07fef15e231ad06fbde953dfcaa
nitikhon <140893068+nitikhon@users.noreply.github.com>
2026-02-28 21:52:42 +0700

    feat: update corpus to `brown` and using universal tagset for better clarity

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import nltk
-from nltk.corpus import treebank
+from nltk.corpus import brown
 from implement.implement import Viterbi
 
 
 def main():
-    tagged_sents = treebank.tagged_sents()
-    tagged_words = treebank.tagged_words()
+    tagged_sents = brown.tagged_sents(tagset='universal')
+    tagged_words = brown.tagged_words(tagset='universal')
     
     v = Viterbi(tagged_sents, tagged_words)
 
     v.init()
 
-    sentence = ["I", "love", "cats", "."]
+    # sentence = ["I", "love", "cats", "."]
+    # sentence = ["The", "old", "man", "the", "boat", "."]
+    # sentence = ["The", "complex", "houses", "married", "and", "single", "soldiers", "."]
+    sentence = ["Time", "flies", "like", "an", "arrow", ";", "fruit", "flies", "like", "a", "banana", "."]
 
-    path = v.find_tags(sentence)
+    path = v.find_tags(sentence, detailed=True)
 
     print(path)
 
 if __name__ == "__main__":
-    nltk.download('treebank')
-    nltk.download('tagsets_json')
+    nltk.download('brown')
+    nltk.download('universal_tagset')
     main()
